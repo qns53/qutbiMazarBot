226d7ccf14333703e89c8ea3c3552db62932d556
qns53 <kid.huzkot@gmail.com>
2020-10-25 12:27:34 +0530

    25-5

--- dbTest.py
+++ dbTest.py
@@ -7,13 +7,18 @@ db = pymysql.connect("localhost","root","yaahusain","demo1")
 
 cursor = db.cursor()
 
-sql = """CREATE TABLE EMPLOYEE (
-   FIRST_NAME  CHAR(20) NOT NULL,
-   LAST_NAME  CHAR(20),
-   AGE INT,
-   INCOME FLOAT )"""
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+   LAST_NAME, AGE,INCOME)
+   VALUES ('Mac', 'Mohan', 20, 2000.23)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
 
-cursor.execute(sql)
 print(db)
 # disconnect from server
 db.close()
