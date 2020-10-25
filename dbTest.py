9e8e27467984bdeb2d0eff27b81e590b37a098ec
qns53 <kid.huzkot@gmail.com>
2020-10-25 11:59:41 +0530

    25-3

--- dbTest.py
+++ dbTest.py
@@ -3,7 +3,16 @@
 import pymysql
 
 # Open database connection
-db = pymysql.connect("localhost","root","yaahusain")
+db = pymysql.connect("localhost","root","yaahusain","demo1")
 
+sql = """CREATE TABLE EMPLOYEE (
+   FIRST_NAME  CHAR(20) NOT NULL,
+   LAST_NAME  CHAR(20),
+   AGE INT,
+   INCOME FLOAT )"""
 
+cursor.execute(sql)
 print(db)
+# disconnect from server
+db.close()
+
